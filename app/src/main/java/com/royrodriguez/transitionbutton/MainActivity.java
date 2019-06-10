ae29fb625255438f6e25162bbbe9d092be06b041
Roy Rodriguez <roy@Roys-MBP.localdomain>
2019-06-10 10:15:14 -0500

    refactor: Changed gif`s.

--- app/src/main/java/com/royrodriguez/transitionbutton/MainActivity.java
+++ app/src/main/java/com/royrodriguez/transitionbutton/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        boolean isSuccessful = true;
+                        boolean isSuccessful = false;
 
                         if (isSuccessful) {
                             transitionButton.stopAnimation(TransitionButton.StopAnimationStyle.EXPAND, new TransitionButton.OnAnimationStopEndListener() {
